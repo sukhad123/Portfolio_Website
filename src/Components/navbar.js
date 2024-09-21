41f33a9e822a02a4ed35b0b16ff4d17645d76075
sukhad123 <sukhadadhikari3@gmai.com>
2024-09-21 13:44:41 -0400

    Recommit changes

---
+++ src/Components/navbar.js
@@ -0,0 +1,22 @@
+ 
+
+export default function Navbar(){{
+
+       
+    return<>
+    <div className = "customDesign">
+ <div className="flex flex-row  ">
+  <div className="basis-1/2">
+  <h1>Hi,</h1>
+  <h1>I am Sukhad</h1>
+  <h1>Aspiring👨‍💻 <br></br>Software Developer</h1>
+  </div>
+  < div
+   className="basis-1/2 ">
+   
+  <img  className = "imageClass"src="https://scontent.fyto1-2.fna.fbcdn.net/v/t39.30808-6/279062631_1148583269326559_3585516563066002083_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=LvxMlwHJeSoQ7kNvgGUXLkU&_nc_ht=scontent.fyto1-2.fna&_nc_gid=ACZdXAA33mCML-6WG1pnkm_&oh=00_AYC5XhxzFD89rzeygdADarovfMJ9FhWBrfctw3ZHl7Tfvw&oe=66F44DFB"   alt ="Error loading the image"></img></div>
+    </div>
+  
+</div>
+    </>
+}}

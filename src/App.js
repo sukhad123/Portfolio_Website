f51cdee1e230bfbe8a3f27cd8636845710eaa1b2
sukhad123 <sukhadadhikari3@gmai.com>
2024-10-22 00:13:05 -0400

    added loading

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from  "./Components/navbar"
-import {Example} from "./Components/techStack"
+import {Example} from "./Components/techStack";
+import Loading from "./loading";
+import { useEffect, useState } from "react";
 
 function App() {
+ 
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+      setTimeout(() => setLoading(false), 3300)
+  }, [])
+  if (loading) {
+      return <Loading/>
+  }
   return (
     <div>
   <Example/>
